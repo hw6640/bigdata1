ca86acef2e75aac601a634e700918daf89916b40
hyewon <hyewon@DESKTOP-1799EFV>
2020-09-07 16:39:51 +0900

    길동이 수정 요망

--- webmvc/src/kr/smhrd/MyUtill.java
+++ webmvc/src/kr/smhrd/MyUtill.java
@@ -2,10 +2,11 @@ package kr.smhrd;
 
 public class MyUtill {
 	
-	// 한글 수정 깨짐 
+	// 한글 수정 깨짐 !
 	public int hap() {
 		int sum = 0;
 		
+		// 길동아 여기 좀 수정해봐 ?
 		for(int i = 0; i <= 10; i++) {
 			sum = sum + i;
 		}
